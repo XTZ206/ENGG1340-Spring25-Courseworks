c20be7b7eddb58bfde2f7b67b93565c36494d1a9
XTZ206 <ywx2006@outlook.com>
2025-04-11 10:38:15 +0800

    assignment: assignment 3 question 2 test script fix bug

--- assignments/assignment3/question2/tests/test.sh
+++ assignments/assignment3/question2/tests/test.sh
@@ -8,7 +8,7 @@ echo "Running Provided Testcases"
 for i in {1..1}
 do
     echo "Test $i"
-    ./route "inputs/input2_$i.txt" > "outputs/output2_$i.txt"
+    echo "inputs/input2_$i.txt" | ./route > "outputs/output2_$i.txt"
     diff "outputs/output2_$i.txt" "expected/output2_$i.txt"
 done
 
